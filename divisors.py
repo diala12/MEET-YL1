b117045b9883e10f059a24d048ba51734d18001f
brit13-meet <brit13@meet.mit.edu>
2016-01-13 19:59:46 +0200

    oop

---
+++ divisors.py
@@ -0,0 +1,8 @@
+def divisors(n=int (input("please write a number"))):
+	for num in range(1,n+1):
+		if (n % num==0):
+			print(num)
+divisors()
+
+
+
---
+++ yousef.py
@@ -0,0 +1,6 @@
+from animalfile import Animal
+dog=Animal("max","Brown",2)
+cat=Animal("min","white",17)
+
+print(dog.name+"is"+str(dog.age)+"years old and its color is"+dog.color)
+print(dog.malak())
